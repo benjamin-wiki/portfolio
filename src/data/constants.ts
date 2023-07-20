86235745d1892938f5156d5d961ce908a28471a9
Benjamin Wiki <benjaminwiki99@gmail.com>
2023-07-20 19:39:13 +1200

    update

---
+++ src/data/constants.ts
@@ -0,0 +1,22 @@
+export const MENU_LINKS = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "About",
+		path: "/about/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+];
+
+// ! Remember to add your own socials
+export const SOCIAL_LINKS = {
+	github: "https://github.com/benjamin-wiki",
+	twitter: "https://www.twitter.com",
+	linkedin: "https://www.linkedin.com/in/bwiki/",
+	email: "benjaminwiki99@gmail.com",
+};
